62e1a7b4740381e24855a8c70e331b2549314469
COMP2101PSB <singhbajwa1509@gmail.com>
2019-09-17 14:46:25 -0700

    ok

--- bash/helloworldugly.sh
+++ bash/helloworldugly.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo -n "helb wold" |
+echo -n "he1b wold" |
   sed -e "s/b/o/g" -e "s/1/11/" -e "s/ol/orl" |
   tr "h" "H"|
   tr "w" "W"|
   awk '{print $1 "\x20" $2 "\41"}'
 bc <<< "(($$ * 4 - 24)/2 + 12)/2" |
   sed 's/^/I am process # /'
-  
