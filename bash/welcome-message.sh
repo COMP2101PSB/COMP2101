c232a83d29e38207769dedb13094645d662c12c1
COMP2101PSB <singhbajwa1509@gmail.com>
2019-09-27 06:28:01 -0700

    F

---
+++ bash/welcome-message.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# This script produces the dynamic welcome message
+# it should look like
+#   Welcome to planet hostname, title name!
+
+# Improve this script by using the value in the automatic variable $USER for the name
+# Improve this script by adding some time and day of the week information to the welcome message
+#   Use a format like this:
+#   It is HH:MM AM on weekday.
+
+###############
+# Variables   #
+###############
+title="Overlord"
+myname="dennis"
+hostname=$(hostname)
+
+###############
+# Main        #
+###############
+cat <<EOF
+
+Welcome to planet $hostname, "$title $myname!"
+
+EOF
