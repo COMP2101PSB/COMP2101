7b7ec8a716ca12202c31564f4aa2ad2042636899
COMP2101PSB <singhbajwa1509@gmail.com>
2019-09-26 23:16:53 -0700

    k v

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -7,10 +7,12 @@
 # Improve this script by demonstrating the modulus operation
 #   - the output should look something like:
 #   - first divided by second gives X with a remainder of Y
-# Improve this script by calculating and displaying the first number raised to the power of the second number
+# Improve this script by calculating and displaying the first number raised to the power of the second numberech
+echo "Please enter a desired number"
+read firstnum
+echo "Please enter another number"
+read secondnum
 
-firstnum=5
-secondnum=2
 sum=$((firstnum + secondnum))
 dividend=$((firstnum / secondnum))
 fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
