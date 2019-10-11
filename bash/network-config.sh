a5a8d02f77cd0a1af6373bdb455a50f7237a74e9
COMP2101PSB <singhbajwa1509@gmail.com>
2019-10-10 22:24:49 -0700

    f

--- bash/network-config.sh
+++ bash/network-config.sh
@@ -34,7 +34,7 @@
 
 MyHostname=$(hostname)
 LanAddress=$(ip a s "$(ip a |awk '/: e/{gsub(/:/,"");print $2}')"|awk '/inet /{gsub(/\/.*/,"");print $2}')
-LanHostname=$(getent hosts $(ip a s $(ip a |awk '/: e/{gsub(/:/,"");print $2}'))|awk '/inet /{gsub(/\/.*/,"");print $2}' | awk '{print $2}')
+LanHostname=$(getent hosts "$(ip a s "$(ip a |awk '/: e/{gsub(/:/,"");print $2}')")"|awk '/inet /{gsub(/\/.*/,"");print $2}' | awk '{print $2}')
 ExternalIpAddress=$(curl -s icanhazip.com)
 ExternalName=$(getent hosts "$(curl -s icanhazip.com)" | awk '{print $2}')
 RouterAddress=$(ip route show | grep -i 'default via'| awk '{print $3 }')
