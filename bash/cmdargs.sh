a0938df06e88e76d2c4cd41207028a1b8935de63
COMP2101PSB <singhbajwa1509@gmail.com>
2019-10-17 16:29:52 -0700

    tgt

--- bash/cmdargs.sh
+++ bash/cmdargs.sh
@@ -3,6 +3,7 @@
 
 # loop through the command line arguments
 while [ $# -gt 0 ]; do
+  echo "Processing '$1'."
   # tell the user how many things are left on the command line
   echo "There are $# things left to process on the command line."
   # display whatever is in $1
@@ -12,11 +13,32 @@ while [ $# -gt 0 ]; do
 #             recognizes the following command line options and tells the user if
 #             they were found on the command line
 #          Options to recognize: -h for help, -v for verbose mode
+case $1 in
+  -h )
+  echo '"-h" is for help'
+  ;;
+  -v )
+  echo '"-v" is for verbose'
+  ;;
 # TASK 2: add recognition of a debug option -d that expects a number that specifies the debug level (e.g. -d 3)
 #             save the debug level in a variable to be available to use later in the script
 #             display an error if the user gave the -d option without a number after it
 # TASK 3: put anything that wasn't recognized on the command line into a variable for use later in the script
-  echo "Processing '$1'."
+  -d )
+    case "$2" in
+    [0-9] )
+    echo "-d for debug level $2"
+    shift
+    ;;
+    *)
+    echo "The -d option should be followed by a number between (0-9)"
+    esac
+    ;;
+    *)
+    errors=$1
+    echo  "Error: unknown value $errors"
+    ;;
+esac
 
   # each time through the loop, shift the arguments left
   # this decrements the argument count for us
