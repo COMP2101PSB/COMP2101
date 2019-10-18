02756173219564fce4bd38138f9b0f1431a6a12a
COMP2101PSB <singhbajwa1509@gmail.com>
2019-10-18 06:35:08 -0700

    n

--- bash/login-welcome.sh
+++ bash/login-welcome.sh
@@ -24,7 +24,7 @@ Day=$(date +%p)
 
 ###############
 # Main        #
-###############
+############  ###
 cow="$(cat <<EOF
 Welcome to planet $hostname, "$title $myname!". The time is $Hour:$Minutes$Day and it is a $TimeOFDay.
 EOF
